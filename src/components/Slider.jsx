dcaf6b6be6e65da93d984802b450b7fd1164ed2b
Noel Banas <noel.banas@gmail.com>
2022-07-15 10:18:40 +0800

    Change currency symbol
    Change currency from '$' to 'RM'

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -76,7 +76,7 @@ function Slider() {
                             >
                                 <p className='swiperSlideText'>{data.name}</p>
                                 <p className='swiperSlidePrice'>
-                                    ${data.discountedPrice ?? data.regularPrice}{' '}
+                                    RM{data.discountedPrice ?? data.regularPrice}{' '}
                                     {data.type === 'rent' && '/ month'}
                                 </p>
                             </div>
